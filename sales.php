69897a14b61175304ea09f59b81cf90eca4d4e86
BENILIN <benilin@nurtureu.tech>
2024-08-01 19:15:48 +0400

    Updating sales page

--- sales.php
+++ sales.php
@@ -50,40 +50,51 @@
                                     </thead>
                                 </table>
                             </div>
-                            <div class="flex-grow-1" >
-                                <div class="h-100 overflow-auto" style="height:60vh !important ">
-                                    <table class="table table-hover table-striped bg-light" id="plist">
-                                        <colgroup>
-                                            <col width="25%">
-                                            <col width="20%">
-                                            <col width="25%">
-                                            <col width="15%">
-                                            <col width="15%">
-                                        </colgroup>
-                                        <tbody>
-                                        <?php 
-                                        $sql = "SELECT p.*,c.name as cname FROM `product_list` p inner join `category_list` c on p.category_id = c.category_id where p.status = 1 and p.delete_flag = 0 order by p.`name` asc";
-                                        $qry = $conn->query($sql);
-                                        while($row = $qry->fetch_assoc()):
-                                            $stock_in = $conn->query("SELECT sum(quantity) as `total` FROM `stock_list` where unix_timestamp(`expiry_date`) >= unix_timestamp(CURRENT_TIMESTAMP) and id = '{$row['id']}' ")->fetch_assoc()['total'];
-                                            $stock_out = $conn->query("SELECT sum(quantity) as `total` FROM `transaction_items` where id = '{$row['id']}' ")->fetch_assoc()['total'];
-                                            $stock_in = $stock_in > 0 ? $stock_in : 0;
-                                            $stock_out = $stock_out > 0 ? $stock_out : 0;
-                                            $qty = $stock_in-$stock_out;
-                                            $qty = $qty > 0 ? $qty : 0;
-                                        ?>
-                                        <tr class="item <?php echo $qty < 50? "bg-danger bg-opacity-25":'' ?>" data-id="<?php echo $row['id'] ?>">
-                                            <td class="td py-0 px-1 pname"><?php echo $row['cname'] ?></td>
-                                            <td class="td py-0 px-1 pcode"><?php echo $row['product_code'] ?></td>
-                                            <td class="td py-0 px-1 name"><?php echo $row['name'] ?></td>
-                                            <td class="td py-0 px-1 text-end price"><?php echo format_num($row['cakeprice']) ?></td>
-                                            <td class="td py-0 px-1 text-end qty"><?php echo $qty ?></td>
-                                        </tr>
-                                        <?php endwhile; ?>
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
+                            <div class="h-100 overflow-auto" style="height:60vh !important;">
+    <table class="table table-hover table-striped bg-light" id="plist">
+        <colgroup>
+            <col width="25%">
+            <col width="20%">
+            <col width="25%">
+            <col width="15%">
+            <col width="15%">
+        </colgroup>
+        <tbody>
+        <?php 
+        $sql = "SELECT p.*, c.name as cname FROM `product_list` p 
+                INNER JOIN `category_list` c ON p.category_id = c.category_id 
+                WHERE p.status = 1 AND p.delete_flag = 0 
+                ORDER BY p.`name` ASC";
+        $qry = $conn->query($sql);
+        while($row = $qry->fetch_assoc()):
+            $stock_in = $conn->query("SELECT SUM(quantity) as `total` 
+                                      FROM `stock_list` 
+                                      WHERE UNIX_TIMESTAMP(`expiry_date`) >= UNIX_TIMESTAMP(CURRENT_TIMESTAMP) 
+                                      AND id = '{$row['id']}'")->fetch_assoc();
+            $stock_out = $conn->query("SELECT SUM(quantity) as `total` 
+                                       FROM `transaction_items` 
+                                       WHERE id = '{$row['id']}'")->fetch_assoc();
+
+            $stock_in_total = isset($stock_in['total']) && is_numeric($stock_in['total']) ? $stock_in['total'] : 0;
+            $stock_out_total = isset($stock_out['total']) && is_numeric($stock_out['total']) ? $stock_out['total'] : 0;
+
+            $qty = $stock_in_total - $stock_out_total;
+            $qty = $qty > 0 ? $qty : 0;
+        ?>
+        <tr class="item <?php echo $qty < 50 ? 'bg-danger bg-opacity-25' : '' ?>" data-id="<?php echo $row['id'] ?>">
+            <td class="td py-0 px-1 pname"><?php echo htmlspecialchars($row['cname']) ?></td>
+            <td class="td py-0 px-1 pcode"><?php echo htmlspecialchars($row['product_code']) ?></td>
+            <td class="td py-0 px-1 name"><?php echo htmlspecialchars($row['name']) ?></td>
+            <td class="td py-0 px-1 text-end price"><?php echo format_num($row['cakeprice']) ?></td>
+            <td class="td py-0 px-1 text-end qty"><?php echo $qty ?></td>
+        </tr>
+        <?php endwhile; ?>
+        </tbody>
+    </table>
+</div>
+
+
+
                         </div>
                     </div>
                     <div class="col-4 h-100 py-2">
@@ -151,151 +162,161 @@
 </div>
 
 <script>
-    $(function(){
-        $('#search').on('input',function(){
-            var _search = $(this).val().toLowerCase()
-            $('#plist tbody tr').each(function(){
-                var _text = $(this).text().toLowerCase()
-                if(_text.includes(_search) === true){
-                    $(this).toggle(true)
-                }else{
-                    $(this).toggle(false)
-                }
-            })
-        })
-        $('#plist tbody tr').click(function(){
-            var _tr = $(this);
-            var pid = _tr.attr('data-id')
-            var cname = _tr.find('.cname').text()
-            var pcode = _tr.find('.pcode').text()
-            var name = _tr.find('.name').text()
-            var cakeprice = _tr.find('.cakeprice').text().replace(/,/gi,'')
-            var max = _tr.find('.qty').text()
-            var qty = 1
-            if($('#item-list tbody tr[data-id="'+pid+'"]').length > 0){
-                qty += parseFloat($('#item-list tbody tr[data-id="'+pid+'"]').find('[name="quantity[]"]').val())
-                $('#item-list tbody tr[data-id="'+pid+'"]').find('[name="quantity[]"]').val(qty).trigger('keydown')
-                return false;
+   $(function(){
+    $('#search').on('input', function(){
+        var _search = $(this).val().toLowerCase();
+        $('#plist tbody tr').each(function(){
+            var _text = $(this).text().toLowerCase();
+            if(_text.includes(_search) === true){
+                $(this).toggle(true);
+            } else {
+                $(this).toggle(false);
             }
-            var ntr  = $("<tr tabindex='0'>")
-                ntr.attr('data-id',pid)
-                ntr.append('<td class="py-0 px-1 align-middle"><input class="w-100 text-center" type="number" name="quantity[]" min="1" value="'+qty+'"/>'+
-                        '<input type="hidden" name="id[]" value="'+pid+'"/>'+
-                        '<input type="hidden" name="cakeprice[]" value="'+cakeprice+'"/>'+
-                '</td>')
-                ntr.append('<td class="py-0 px-1 align-middle"><div class="fs-6 mb-0 lh-1">'+pcode+'<br/>'+
-                            '<span class="name">'+name+'</span></br>'+
-                            '(<span class="cakeprice">'+parseFloat(cakeprice).toLocaleString('en-US',{style:'decimal',maximumFractionDigits:2})+'</span>)</div>'+
-                            '</td>');
-            ntr.append('<td class="py-0 px-1 align-middle text-end total">'+parseFloat(cakeprice).toLocaleString('en-US',{style:'decimal',maximumFractionDigits:2})+'</td>')
-            $('#item-list tbody').append(ntr)
-            compute(ntr)
-            calculate_total()
-        })
+        });
+    });
 
-        $('#transaction-save-btn').click(function(){
-            if($('#item-list tbody tr').length <= 0){
-                alert("Please add atleast 1 item first.")
-                return false;
-            }
-            uni_modal("Payment","tender_amount.php?amount="+$('[name="total"]').val())
-        })
-        $('#transaction-form').submit(function(e){
-            e.preventDefault()
-            $('#transaction-save-btn').attr('disabled',true)
-            $('.pop_msg').remove()
-            var _this = $(this)
-            var _el = $('<div>')
-                _el.addClass('pop_msg')
-            $.ajax({
-                url:'./Actions.php?a=save_transaction',
-                data: new FormData($(this)[0]),
-                cache: false,
-                contentType: false,
-                processData: false,
-                method: 'POST',
-                type: 'POST',
-                dataType: 'json',
-                error:err=>{
-                    console.log(err)
-                    _el.addClass('alert alert-danger')
-                    _el.text("An error occurred.")
-                    _this.prepend(_el)
-                    _el.show('slow')
-                    $('#transaction-save-btn').attr('disabled',false)
-                },
-                success:function(resp){
-                    if(resp.status == 'success'){
-                        setTimeout(() => {
-                            uni_modal("RECEIPT","view_receipt.php?id="+resp.transaction_id)
-                        }, 1000);
-                    }else{
-                        _el.addClass('alert alert-danger')
-                    }
-                    _el.text(resp.msg)
-
-                    _el.hide()
-                    _this.prepend(_el)
-                    _el.show('slow')
-                    $('#transaction-save-btn').attr('disabled',false)
-                }
-            })
-        })
-        $('#transaction-form input').keydown(function(e){
-            if(e.which == 13){
-            e.preventDefault()
-            return false
-            }
-        })
-    })
-    function compute(_this){
-        _this.find('[name="quantity[]"]').on('input keydown',function(){
-            var qty = $(this).val() > 0 ? $(this).val() : 0;
-            var cakeprice = _this.find('[name="cakeprice[]"]').val()
-            var _total = parseFloat(qty) * parseFloat(cakeprice)
-
-            _this.find('.total').text(parseFloat(_total).toLocaleString('en-US',{style:'decimal',maximumFractionDigits:2}))
-            calculate_total()
-        })
-        _this.find('[name="quantity[]"]').on('focusout',function(){
-            if($(this).val() <= 0){
-                $(this).val('0')
-            }
-        })
-        _this.on('focusin',function(){
-            $(this).addClass("bg-primary bg-opacity-50 selected-item")
-            $('#remove-item').attr('disabled',false)
-        })
-        _this.on('focusout',function(){
-            if($('#remove-item').is(':focus') == true || $('#remove-item').is(':hover') == true)
+    $('#plist tbody tr').click(function(){
+        var _tr = $(this);
+        var pid = _tr.attr('data-id');
+        var cname = _tr.find('.cname').text();
+        var pcode = _tr.find('.pcode').text();
+        var name = _tr.find('.name').text();
+        var cakeprice = _tr.find('.price').text().replace(/,/gi,'');
+        var max = _tr.find('.qty').text();
+        var qty = 1;
+        
+        if($('#item-list tbody tr[data-id="'+pid+'"]').length > 0){
+            qty += parseFloat($('#item-list tbody tr[data-id="'+pid+'"]').find('[name="quantity[]"]').val());
+            $('#item-list tbody tr[data-id="'+pid+'"]').find('[name="quantity[]"]').val(qty).trigger('keydown');
             return false;
-            $(this).removeClass("bg-primary bg-opacity-50 selected-item")
-            $('#remove-item').attr('disabled',true)
-        })
-        $('#transaction-form input').keydown(function(e){
-            if(e.which == 13){
-            e.preventDefault()
-            return false
-            }
-        })
-    }
-    function calculate_total(){
-        var sub = 0
-        var total = 0
-        var discount = 0
-        $('#item-list tr .total').each(function(){
-            val = $(this).text().replace(/,/gi,'')
-            sub += parseFloat(val)
-        })
-        $('[name="total"]').val(parseFloat(sub))
-        $('#total').text(parseFloat(sub).toLocaleString('en-US',{style:'decimal',manimumFractionDigits:2,maximumFractionDigits:2}))
-        $('#subTotal').text(parseFloat(sub).toLocaleString('en-US',{style:'decimal',manimumFractionDigits:2,maximumFractionDigits:2}))
-        discount = sub * .12;
-        $('#tax').text(parseFloat(discount).toLocaleString('en-US',{style:'decimal',manimumFractionDigits:2,maximumFractionDigits:2}))
-    }
-    function remove_item(){
-        $('#item-list tr.selected-item').remove()
-        calculate_total()
-        $('#remove-item').attr('disabled',true)
         }
+        
+        var ntr  = $("<tr tabindex='0'>")
+            .attr('data-id', pid)
+            .append('<td class="py-0 px-1 align-middle"><input class="w-100 text-center" type="number" name="quantity[]" min="1" value="'+qty+'"/>'+
+                    '<input type="hidden" name="id[]" value="'+pid+'"/>'+
+                    '<input type="hidden" name="cakeprice[]" value="'+cakeprice+'"/>'+
+            '</td>')
+            .append('<td class="py-0 px-1 align-middle"><div class="fs-6 mb-0 lh-1">'+pcode+'<br/>'+
+                    '<span class="name">'+name+'</span></br>'+
+                    '(<span class="cakeprice">'+parseFloat(cakeprice).toLocaleString('en-US', {style:'decimal', maximumFractionDigits:2})+'</span>)</div>'+
+                    '</td>')
+            .append('<td class="py-0 px-1 align-middle text-end total">'+parseFloat(cakeprice).toLocaleString('en-US', {style:'decimal', maximumFractionDigits:2})+'</td>');
+        $('#item-list tbody').append(ntr);
+        compute(ntr);
+        calculate_total();
+    });
+
+    $('#transaction-save-btn').click(function(){
+        if($('#item-list tbody tr').length <= 0){
+            alert("Please add at least 1 item first.");
+            return false;
+        }
+        uni_modal("Payment", "tender_amount.php?amount="+$('[name="total"]').val());
+    });
+
+    $('#transaction-form').submit(function(e){
+        e.preventDefault();
+        $('#transaction-save-btn').attr('disabled', true);
+        $('.pop_msg').remove();
+        var _this = $(this);
+        var _el = $('<div>').addClass('pop_msg');
+        $.ajax({
+            url: './Actions.php?a=save_transaction',
+            data: new FormData($(this)[0]),
+            cache: false,
+            contentType: false,
+            processData: false,
+            method: 'POST',
+            type: 'POST',
+            dataType: 'json',
+            error: err => {
+                console.log(err);
+                _el.addClass('alert alert-danger');
+                _el.text("An error occurred.");
+                _this.prepend(_el);
+                _el.show('slow');
+                $('#transaction-save-btn').attr('disabled', false);
+            },
+            success: function(resp){
+                if(resp.status == 'success'){
+                    setTimeout(() => {
+                        uni_modal("RECEIPT", "view_receipt.php?id="+resp.transaction_id);
+                    }, 1000);
+                } else {
+                    _el.addClass('alert alert-danger');
+                }
+                _el.text(resp.msg);
+                _el.hide();
+                _this.prepend(_el);
+                _el.show('slow');
+                $('#transaction-save-btn').attr('disabled', false);
+            }
+        });
+    });
+
+    $('#transaction-form input').keydown(function(e){
+        if(e.which == 13){
+            e.preventDefault();
+            return false;
+        }
+    });
+});
+
+function compute(_this){
+    _this.find('[name="quantity[]"]').on('input keydown', function(){
+        var qty = $(this).val() > 0 ? $(this).val() : 0;
+        var cakeprice = _this.find('[name="cakeprice[]"]').val();
+        var _total = parseFloat(qty) * parseFloat(cakeprice);
+
+        _this.find('.total').text(parseFloat(_total).toLocaleString('en-US', {style:'decimal', maximumFractionDigits:2}));
+        calculate_total();
+    });
+
+    _this.find('[name="quantity[]"]').on('focusout', function(){
+        if($(this).val() <= 0){
+            $(this).val('0');
+        }
+    });
+
+    _this.on('focusin', function(){
+        $(this).addClass("bg-primary bg-opacity-50 selected-item");
+        $('#remove-item').attr('disabled', false);
+    });
+
+    _this.on('focusout', function(){
+        if($('#remove-item').is(':focus') == true || $('#remove-item').is(':hover') == true)
+            return false;
+        $(this).removeClass("bg-primary bg-opacity-50 selected-item");
+        $('#remove-item').attr('disabled', true);
+    });
+
+    $('#transaction-form input').keydown(function(e){
+        if(e.which == 13){
+            e.preventDefault();
+            return false;
+        }
+    });
+}
+
+function calculate_total(){
+    var sub = 0;
+    var total = 0;
+    var discount = 0;
+    $('#item-list tr .total').each(function(){
+        var val = $(this).text().replace(/,/gi, '');
+        sub += parseFloat(val);
+    });
+    $('[name="total"]').val(parseFloat(sub));
+    $('#total').text(parseFloat(sub).toLocaleString('en-US', {style:'decimal', maximumFractionDigits:2}));
+    $('#subTotal').text(parseFloat(sub).toLocaleString('en-US', {style:'decimal', maximumFractionDigits:2}));
+    discount = sub * .12;
+    $('#tax').text(parseFloat(discount).toLocaleString('en-US', {style:'decimal', maximumFractionDigits:2}));
+}
+
+function remove_item(){
+    $('#item-list tr.selected-item').remove()
+    calculate_total()
+    $('#remove-item').attr('disabled',true)
+    }
 </script>
